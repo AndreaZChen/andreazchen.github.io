1461ba4089e10144b991545bace1b2cf3feb05ad
Andrea Chen <andrea.zonghao.chen@gmail.com>
2019-09-15 17:33:00 +0200

    Add scripting to handle multiple forcefield prisoners

---
+++ js/bouncingBadScript.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var bouncingWords = [
+  "Transphobia",
+  "Homophobia",
+  "Ableism",
+  "Marmite",
+  "Toxic masculinity",
+  "Racism",
+  "Repetitive strain injuries",
+];
+
+function addBouncingWordsToHtml() {
+  var newHtml = "";
+  bouncingWords.forEach(function(word) {
+    var delay = Math.random() * 1000;
+    newHtml +=
+      `<div
+        class="bouncing-words-wrapper"
+        style="animation-delay: -${delay}s"
+      >
+        <div style="animation-delay: -${delay}s">
+          <div style="animation-delay: -${delay}s">
+            <span style="animation-delay: -${delay}s">
+              ${word}
+            </span>
+          </div>
+        </div>
+      </div>`;
+  });
+  
+  document.getElementById("bounce-area").innerHTML = newHtml;
+};
+
+addBouncingWordsToHtml();
